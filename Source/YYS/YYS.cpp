db625d3fc86a077f3eb4b190d8cd4074aaface98
zhuangbi <37168636+zhuangbiiii@users.noreply.github.com>
2023-01-18 08:26:07 +0800

    initialize

---
+++ Source/YYS/YYS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "YYS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, YYS, "YYS" );
---
+++ Source/YYS/YYSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "YYSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YYS_API AYYSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
