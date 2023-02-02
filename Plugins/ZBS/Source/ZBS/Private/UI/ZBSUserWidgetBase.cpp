ca965c4fb9adee70b99491d97b0521ea1429c9d4
zhuangbiiii <zhuangbiiii@outlook.com>
2023-02-02 14:39:59 +0800

    Revert "ZBS Plugins"
    This reverts commit 25d817a1771abb3a4c65ae72574e85a4bfc96413.

--- Plugins/ZBS/Source/ZBS/Private/UI/ZBSUserWidgetBase.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "UI/ZBSUserWidgetBase.h"
-
-bool UZBSUserWidgetBase::Initialize()
-{
-    return false;
-}
